073804f7a4b9ae4c8fb8733804d332765ae57238
shafayetullah7 <shafayet769@gmail.com>
2023-11-24 20:07:57 +0600

    db prettier eslint

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log('hello world');
+import express from 'express'
+import cors from 'cors';
+const app = express()
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+export default app;
